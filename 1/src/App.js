76cf7c53c8f43fb7ee4e5855a5e579fad2facfce
SungHyun Yu <ysh1513@naver.com>
2019-12-29 17:47:34 +0900

    spa 라우터 맛보기

--- 1/src/App.js
+++ 1/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import Home from './components/Home.js'
+import About from './components/About.js'
 
-import MyComponent1 from './MyComponent1.js'
-import MyComponent2 from './MyComponent2.js'
-
+import { Route,Link } from "react-router-dom"
 
 function App() {
   return (
     <div>
       <h1>hello react</h1>
-      <MyComponent1/>
-      <MyComponent2/>
+
+      <Link to="/">[Home]</Link>
+      <Link to="/about">[About]</Link>
+      <hr/>
+      
+      <Route path="/" component={Home} exact={true}/>
+      <Route path="/about" component={About}/>
+
     </div>
   );
 }
