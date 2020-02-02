1eb8a391a7943bb9d74ecd77716b98d87a6dd2c3
SungHyun Yu <ysh1513@naver.com>
2020-02-02 15:28:26 +0900

    온라인신문을 만들어보자

---
+++ 6/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Route, NavLink, Switch} from 'react-router-dom'
+import './assets/css/style.css';
+import NewsList from './components/NewsList'
+import Category from './components/Category'
+import NewsPage from './pages/NewsPage'
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
