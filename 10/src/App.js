f89af8ba7f3b0965216567ddd64b74c14c129a41
SungHyun Yu <ysh1513@naver.com>
2020-02-23 10:45:54 +0900

    9번은 thunk 연습 10번이 실습?

---
+++ 10/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {NavLink,Switch,Route} from 'react-router-dom'
+
+
+function App() {
+
+  const myStyle = {
+    fontWeight: 'bold',
+    color: '#b82514',
+    textDecoration: 'none'
+  };
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
